d1e0f12d145658a98c38d2d4d656f734d5715b21
yoanp <pouliart.yoan@gmail.com>
2025-05-05 12:21:56 +0200

    add filter

--- src/main.py
+++ src/main.py
@@ -10,23 +10,24 @@ app = Flask(__name__,
             template_folder=os.path.join(BASE_DIR, 'templates'),
             static_folder=os.path.join(BASE_DIR, 'static'))
 
-tasks = read_tasks_file()
-
 @app.route("/")
 def home():
+    tasks = read_tasks_file()
     return render_template("index.html", tasks=tasks)
 
 @app.route("/add", methods=["POST"])
 def add_task():
     dt = datetime.now().strftime("%d-%m-%Y %H:%M")
-    new_task = {"id" : len(tasks), "text" : request.form.get("newTask"), "date" : dt, "done" : False}
-    if new_task:
+    new_task = {"text" : request.form.get("newTask"), "date" : dt, "done" : False}
+    if new_task["text"] != "":
+        tasks = read_tasks_file()
         tasks.append(new_task)
         write_tasks_file(tasks)
     return redirect(url_for("home"))
 
 @app.route("/delete/<int:task_id>", methods=["POST"])
 def delete_task(task_id):
+    tasks = read_tasks_file() 
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
         write_tasks_file(tasks)
@@ -34,12 +35,18 @@ def delete_task(task_id):
 
 @app.route("/toggle/<int:task_id>", methods=["POST"])
 def toggle_done(task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            task["done"] = not task["done"]
-    write_tasks_file(tasks)
+    tasks = read_tasks_file()
+    if 0 <= task_id < len(tasks):
+        tasks[task_id]["done"] = not tasks[task_id]["done"]
+        write_tasks_file(tasks)
     return "", 204
 
+@app.route("/filter", methods=["POST"])
+def filter_tasks():
+    tasks = read_tasks_file()
+    sorted_tasks = sorted(tasks, key=lambda task: task["done"])
+    write_tasks_file(sorted_tasks)
+    return render_template("task_list.html", tasks=sorted_tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
